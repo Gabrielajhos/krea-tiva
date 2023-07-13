4ffd9184006c518c0a553281d7cc1b184ba4ba9a
Gabriela Contreras <gabrielajhos22@gmail.com>
2023-07-13 19:12:11 -0300

    arreglos visuales 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <div className='bg-gray-100 p-8'>
 
-<div className='shadow-2xl  p-4 bg-white block md:flex'>
+<div className='shadow-2xl  p-4 bg-white block md:flex md:space-x-10'>
 
-<nav className='block md:space-x-8 space-x-2 items-center p-4 md:ml-4 text-violet-900 md:text-lg text-sm font-bold font-sans '>
+<nav className='block md:space-x-8 space-x-2 items-center p-4 md:ml-16 text-violet-900 md:text-2xl text-sm font-bold font-sans '>
       <a className='hover:text-orange-500' href='#home'>Home</a>
       <a className='hover:text-orange-500' href='#nosotros'>Nosotros</a>
       <a className='hover:text-orange-500' href='#servicios'>Servicios</a>
@@ -15,11 +15,11 @@ const Footer = () => {
     </nav>
 
 <div>
-    <img className='w-32 m-auto' src={logo} alt='logo krea-tiva'/>
+    <img className='w-32 m-auto ' src={logo} alt='logo krea-tiva'/>
     <h2 className='text-violet-900 text-3xl font-semibold'>Soluciones tecnologicas</h2>
 </div>
     <div>
-      <h3 className='block space-x-10 items-center p-4 ml-4 text-violet-900 text-lg font-bold font-sans '>Terminos y condiciones</h3>
+      <h3 className='block space-x-10 items-center p-4 ml-4 text-violet-900 text-2xl font-bold font-sans '>Terminos y condiciones</h3>
       <h3></h3>
     </div>
 </div>      
